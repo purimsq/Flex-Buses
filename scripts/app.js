b88ff8cb25f26b51cb70298464511f078242175d
Dylen <dylengitaka@gmail.com>
2025-06-17 11:46:31 +0300

    commmit

--- scripts/app.js
+++ scripts/app.js
@@ -394,6 +394,7 @@ function swapCities() {
 }
 
 function searchBuses() {
+    const fromCity = document('fromCity').value;
     const toCity = document.getElementById('toCity').value;
     const departureDate = document.getElementById('departureDate').value;
     const passengers = document.getElementById('passengers').value;
